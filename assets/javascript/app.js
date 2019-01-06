c2cfa2a24d3cf154660b6d689925fd3ed8516fb6
reneecosmic <renee.hickman@outlook.com>
2019-01-06 11:49:04 -0600

    updated questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,54 @@
 var triviaQuestions =
 [
   {
-    question: "Question1?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "A person with an April 13th birthday would have what sign?",
+    answerChoices: ["Scorpio", "Pisces", "Aries", "Taurus"],
     correct: 2
   },
 
   {
-    question: "Question2??",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "Which of the following keywords would a Gemini most likely use to describe themselves?",
+    answerChoices: ["I feel", "I am", "I desire", "I think"],
     correct: 3
   },
   {
-    question: "Question3?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "Which of the four elements is Capricorn?",
+    answerChoices: ["Air", "Earth", "Wind", "Fire"],
     correct: 1
   },
   {
-    question: "Question4?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "Which planet is symbolic of our emotional nature?",
+    answerChoices: ["Sun", "Mars", "Moon", "Mercury"],
     correct: 2
   },
   {
-    question: "Question5?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "The Moon rules which sign of the zodiac?",
+    answerChoices: ["Pisces", "Cancer", "Gemini", "Sagittarius"],
     correct: 1
   },
   {
-    question: "Question6?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "How many houses is the zodiac divided into?",
+    answerChoices: ["12", "6", "10", "4"],
     correct: 0
   },
   {
-    question: "Question7?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "The sign of Libra is ruled by which planet?",
+    answerChoices: ["Mercury", "Pluto", "Venus", "Neptune"],
     correct: 2
   },
   {
-    question: "Question8?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "Which planet controls sex drive and ambition?",
+    answerChoices: ["Mars", "Venus", "Uranus", "Neptune"],
     correct: 0
   },
   {
-    question: "Question9?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "Which keyword best represents the water element?",
+    answerChoices: ["Practical", "Emotional", "Forceful", "Logical"],
     correct: 1
   },
   {
-    question: "Question10?",
-    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    question: "What are the 3 fire signs?",
+    answerChoices: ["Aquarius, Aries, and Sagittarius", "Scorpio, Aries, and Leo", "Capricorn, Pisces, and Aries", "Aries, Sagittarius, and Leo"],
     correct: 3
   }
 ]
