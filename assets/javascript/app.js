5215630bff9cbefa0a4bd4063b1c0e54b2447519
reneecosmic <renee.hickman@outlook.com>
2019-01-06 10:20:47 -0600

    js

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+var triviaQuestions =
+[
+  {
+    question: "Question1?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 2
+  },
+
+  {
+    question: "Question2??",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 3
+  },
+  {
+    question: "Question3?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 1
+  },
+  {
+    question: "Question4?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 2
+  },
+  {
+    question: "Question5?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 1
+  },
+  {
+    question: "Question6?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 0
+  },
+  {
+    question: "Question7?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 2
+  },
+  {
+    question: "Question8?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 0
+  },
+  {
+    question: "Question9?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 1
+  },
+  {
+    question: "Question10?",
+    answerChoices: ["choice1", "choice2", "choice3", "choice4"],
+    correct: 3
+  }
+]
